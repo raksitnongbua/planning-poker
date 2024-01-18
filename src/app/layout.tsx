b0257d8171099f8394e166c33be9536a0894d23e
Tan Raksit <tan.raksit@bitkub.com>
2024-01-18 21:37:31 +0700

    refactor: improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,21 @@ import Layout from '@/components/Layout'
 import { Toaster } from '@/components/ui/toaster'
 import Navbar from '@/components/Navbar'
 import { Analytics } from '@vercel/analytics/react'
-import Head from 'next/head'
 
 const coda = Coda({ weight: '400', display: 'swap', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   verification: {
     google: 'gPypTdWFwPXznnMm9vXjsR0CVA_-qx-JjzBSky6CgLY',
+  },
+  authors: {
+    name: 'Raksit Nongbua',
+  },
+  applicationName: 'Corgi Planning Poker',
+  icons: {
+    icon: 'https://corgi-planning-poker.vercel.app/_next/image?url=%2Fimages%2Fcorgi-logo.png&w=128&q=75',
+    apple:
+      'https://corgi-planning-poker.vercel.app/_next/image?url=%2Fimages%2Fcorgi-logo.png&w=128&q=75',
   },
   title: 'Corgi Planning Poker | Make Estimating Agile Projects',
   description: `Agile teams use this gamified technique to estimate task effort collaboratively,
@@ -25,7 +33,7 @@ export const metadata: Metadata = {
     type: 'website',
   },
   keywords:
-    'planning poker, corgi planning poker,estimating poker, estimate points, agile, planning, Scrum poker, estimate task effort, estimating, service, room, create, efficient, consensus, fostering, agile, gamified',
+    'planning poker, corgi planning poker,estimating poker, estimate points, agile, planning, Scrum poker, estimate task effort, estimating, service, room, create, efficient, consensus, fostering, agile, gamified, corgi game',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
