e7acccce7e6e2825635d2ca83a984c944dc564aa
Tan Raksit <tan.raksit@bitkub.com>
2024-01-19 03:49:35 +0700

    feat: integrated recent rooms api

--- src/components/RecentRooms/RecentRooms.tsx
+++ src/components/RecentRooms/RecentRooms.tsx
@@ -1,16 +1,55 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RoomHistory } from '../RoomHistory'
+import { httpClient } from '@/utils/httpClient'
+import { useLoadingStore, useUserInfoStore } from '@/store/zustand'
+import { Room } from '../RoomHistory/RoomHistory'
+import { useRouter } from 'next/navigation'
 
-export interface RecentRoomsProps {
-  // types...
-}
+const RecentRooms = () => {
+  const [rooms, setRooms] = useState<Room[]>([])
+  const { uid } = useUserInfoStore()
+  const { setLoadingOpen } = useLoadingStore()
+  const router = useRouter()
 
-const RecentRooms: React.FC<RecentRoomsProps> = ({}) => {
+  const transformRoom = (data: any): Room[] => {
+    const rooms: Room[] = data.map((room: any) => ({
+      id: room.id,
+      createdAt: new Date(room.created_at),
+      updatedAt: new Date(room.updated_at),
+      name: room.name,
+      totalMembers: room.members.length,
+    }))
+
+    return rooms
+  }
+
+  useEffect(() => {
+    if (!uid) return
+    const fetchRooms = async () => {
+      try {
+        setLoadingOpen(true)
+        const res = await httpClient.get(`/api/v1/room/recent-rooms/${uid}`)
+        if (res.status === 200) {
+          const newRooms = transformRoom(res.data.data)
+          setRooms(newRooms)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      setLoadingOpen(false)
+    }
+    fetchRooms()
+  }, [setLoadingOpen, uid])
+
+  const handleClickJoinRoom = (roomId: string) => {
+    router.push(`/room/${roomId}`)
+  }
+
   return (
     <main className="px-2 sm:px-8 gap-y-2 items-start max-w-screen-lg mx-auto flex flex-col justify-center">
       <h2 className="text-2xl">RecentRooms</h2>
-      <RoomHistory />
+      <RoomHistory rooms={rooms} onClickJoinRoom={handleClickJoinRoom} />
     </main>
   )
 }
--- src/components/RoomHistory/RoomHistory.tsx
+++ src/components/RoomHistory/RoomHistory.tsx
@@ -13,62 +13,51 @@ import { Button } from '../ui/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
+export interface Room {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  name: string
+  totalMembers: number
+}
 export interface RoomHistoryProps {
+  rooms: Room[]
   onClickJoinRoom?: (roomId: string) => void
 }
 
-const RoomHistory: React.FC<RoomHistoryProps> = ({ onClickJoinRoom }) => {
-  const rooms = [
-    {
-      id: 'mozal-486c1792-d9b8-4336-a97c-1718b394c97f',
-      name: 'TestName',
-      createdAt: '2024-01-17T18:28:22Z',
-      totalMembers: 2,
-      updatedAt: '2024-01-17T18:28:22Z',
-    },
-    {
-      id: 'r3sa1-b34a444d-1bdb-45ac-9667-8c6d47f4e0a4',
-      name: 'TestName2',
-      createdAt: '2024-01-17T18:28:22Z',
-      totalMembers: 2,
-      updatedAt: '2024-01-17T19:47:53Z',
-    },
-  ]
-
+const RoomHistory: React.FC<RoomHistoryProps> = ({ onClickJoinRoom, rooms }) => {
   const handleClickJoinRoom = (id: string) => () => {
     onClickJoinRoom?.(id)
   }
+  const isNotFoundData = rooms.length === 0
 
   return (
     <Table>
-      <TableCaption>A list of your recent rooms.</TableCaption>
+      {isNotFoundData && <TableCaption>Not found your recent rooms.</TableCaption>}
       <TableHeader>
         <TableRow>
           <TableHead className="">Name</TableHead>
           <TableHead className="text-center">Total Members</TableHead>
-          <TableHead className="text-right">Last Used</TableHead>
+          <TableHead className="text-right">Updated Date</TableHead>
           <TableHead className="text-right">Created Date</TableHead>
           <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {rooms.map((room) => (
-          <TableRow key={room.id}>
-            <TableCell className="font-medium">{room.name}</TableCell>
-            <TableCell className="text-center">{room.totalMembers}</TableCell>
-            <TableCell className="text-right">
-              {new Date(room.updatedAt).toLocaleString()}
-            </TableCell>
-            <TableCell className="text-right">
-              {new Date(room.createdAt).toLocaleString()}
-            </TableCell>
-            <TableCell className="text-right">
-              <Button size="icon" onClick={handleClickJoinRoom(room.id)}>
-                <FontAwesomeIcon icon={faRightFromBracket} />
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
+        {!isNotFoundData &&
+          rooms.map((room) => (
+            <TableRow key={room.id}>
+              <TableCell className="font-medium">{room.name}</TableCell>
+              <TableCell className="text-center">{room.totalMembers}</TableCell>
+              <TableCell className="text-right">{room.updatedAt.toLocaleString()}</TableCell>
+              <TableCell className="text-right">{room.createdAt.toLocaleString()}</TableCell>
+              <TableCell className="text-right">
+                <Button size="icon" onClick={handleClickJoinRoom(room.id)}>
+                  <FontAwesomeIcon icon={faRightFromBracket} />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
       </TableBody>
     </Table>
   )
