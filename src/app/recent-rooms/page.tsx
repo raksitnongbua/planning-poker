5ecdf145c5ec8071a0a9c6bcf8e024a59bc3073c
Tan Raksit <tan.raksit@bitkub.com>
2024-01-19 12:59:49 +0700

    refactor: improve seo icon and sitemap

--- src/app/recent-rooms/page.tsx
+++ src/app/recent-rooms/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { RecentRooms as Component } from '@/components/RecentRooms'
 
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Recent Rooms',
+}
+
 const RecentRooms = () => {
   return <Component />
 }
