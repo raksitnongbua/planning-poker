4b657016cbdfb0793fa3a0fdbb755cc40b0a2fb1
tan.raksit <tan.raksit@gmail.com>
2024-01-21 01:09:21 +0700

    refactor: :mag: improve seo and fixes warnning on logs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,34 @@
 'use client'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  const router = useRouter()
-
   return (
-    <header className="px-2 py-1 sm:px-8 sm:pt-4 sm:-pb-2 flex justify-between items-start">
-      <Image
-        className="cursor-pointer"
-        onClick={() => router.push('/')}
-        src="/images/corgi-logo.png"
-        alt="corgi-logo"
-        width={60}
-        height={60}
-      />
-      <a
+    <header className="sm:-pb-2 flex items-start justify-between px-2 py-1 sm:px-8 sm:pt-4">
+      <Link href="/">
+        <Image
+          className="cursor-pointer"
+          src="/images/corgi-logo.png"
+          alt="corgi-logo"
+          width={60}
+          height={60}
+        />
+      </Link>
+      <Link
         className="p-2"
         target="_blank"
         href="https://github.com/raksitnongbua/planning-poker"
         rel="noopener noreferrer"
       >
         <Image
-          className="cursor-pointer"
+          className="size-8 cursor-pointer"
           src="/icons/github.svg"
           alt="github-logo"
-          width={30}
-          height={30}
+          width={0}
+          height={0}
         />
-      </a>
+      </Link>
     </header>
   )
 }
