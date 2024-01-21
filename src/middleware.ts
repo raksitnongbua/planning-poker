e2a984511af166702cd640b896746ff6491eb35c
Raksit Nongbua <tan.raksit@gmail.com>
2024-01-21 17:36:23 +0700

    refactor: :wastebasket: remove unused dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,14 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const hasUID = hasCookie(UID_COOKIE_KEY, { res, req })
   if (!hasUID) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/guest/sign-in`)
-    const jsonRes = await response.json()
-    setCookie(UID_COOKIE_KEY, jsonRes.uuid, { res, req })
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/guest/sign-in`)
+      const jsonRes = await response.json()
+      setCookie(UID_COOKIE_KEY, jsonRes.uuid, { res, req })
+    } catch (error) {
+      console.error(error)
+      return NextResponse.redirect(new URL('/not-found', req.url))
+    }
   }
   return res
 }
