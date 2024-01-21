94f7b4ca6a65f80b473ff192eacdd855b1c85feb
tan.raksit <tan.raksit@gmail.com>
2024-01-21 14:05:22 +0700

    refactor: :recycle: edit check uid exist logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { getCookie, setCookie } from 'cookies-next'
+import { hasCookie, setCookie } from 'cookies-next'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
@@ -6,8 +6,8 @@ import { UID_COOKIE_KEY } from '@/constant/cookies'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const uid = getCookie(UID_COOKIE_KEY, { res, req })
-  if (!uid) {
+  const hasUID = hasCookie(UID_COOKIE_KEY, { res, req })
+  if (!hasUID) {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/guest/sign-in`)
     const jsonRes = await response.json()
     setCookie(UID_COOKIE_KEY, jsonRes.uuid, { res, req })
